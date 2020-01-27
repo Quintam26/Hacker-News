6dcfb522ea1e83fee65d220d39fe54f6ccb25292
Mario <pyrros2000@gmail.com>
2020-01-26 19:59:15 -0800

    correction we grabbed the id of each story/item in order to render 1 specifc story instead all of them

--- pages/item.js
+++ pages/item.js
@@ -1,11 +1,22 @@
 import view from '../utils/view.js';
+import Story from '../components/Story.js'; 
 
-export default function Item() {
-    getStory();
-    view.innerHTML = `<div>item</div>`;  
+export default async function Item() {
+    const story = await getStory();  
+    view.innerHTML = `
+    <div>
+        ${Story(story)}
+    </div>
+    <hr/>
+    `  
 }
 
-function getStory() {
+async function getStory() {
     const storyId = window.location.hash.split('?id=')[1];
-    console.log(storyId);
+    // console.log(storyId);
+    // Path for GET /item/:item
+    const response = await fetch(`https://node-hnapi.herokuapp.com/item/${storyId}`);
+    const story = await response.json();
+    console.log(story);
+    return story; 
 }
