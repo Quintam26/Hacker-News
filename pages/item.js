2841d3c59cdaa29006ebd00d020349e23cd35fd5
Mario <pyrros2000@gmail.com>
2020-02-01 13:14:56 -0800

    comments now render for each story, error cases are handled through an error meassage a user sees, endpoint saved as a utill and exported to othe JS files

--- pages/item.js
+++ pages/item.js
@@ -1,13 +1,31 @@
 import view from '../utils/view.js';
-import Story from '../components/Story.js'; 
+import Story from '../components/Story.js';
+import baseUrl from '../utils/baseUrl.js';
 
 export default async function Item() {
-    const story = await getStory();  
+    let story = null;
+    let hasError = false;
+    let hasComments = false;
+
+    try {
+        story = await getStory();
+        hasComments = story.comments.length > 0;
+    } catch(error) {
+        hasError = true
+        console.log(error);
+    } 
+
+    if(hasError) {
+        view.innerHTML = `<div class="error">Error fetching story</div>`;
+    }
+    
     view.innerHTML = `
     <div>
         ${Story(story)}
     </div>
     <hr/>
+        ${hasComments ? story.comments.map(comment => JSON.stringify(comment)).join('') : 'No Comments'
+    }
     `  
 }
 
@@ -15,7 +33,7 @@ async function getStory() {
     const storyId = window.location.hash.split('?id=')[1];
     // console.log(storyId);
     // Path for GET /item/:item
-    const response = await fetch(`https://node-hnapi.herokuapp.com/item/${storyId}`);
+    const response = await fetch(`${baseUrl}/item/${storyId}`);
     const story = await response.json();
     console.log(story);
     return story; 
